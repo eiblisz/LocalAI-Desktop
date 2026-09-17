2e55f0a93c64599d5e2c24e7c70bc5a6c3d15ba3
eiblisz <eiblisz@gmail.com>
2026-09-17 11:13:58 +0200

    Install verified price equivalence patch

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,13 @@ _install_shopping_search_patch(_web_search_tool)
 
 del _install_shopping_search_patch
 del _web_search_tool
+
+from . import evidence_verifier as _evidence_verifier
+from .answer_verifier_patch import (
+    install_answer_price_equivalence_patch as _install_answer_price_equivalence_patch,
+)
+
+_install_answer_price_equivalence_patch(_evidence_verifier)
+
+del _install_answer_price_equivalence_patch
+del _evidence_verifier
