03123caa4e45a939d54eebe164affc74821a900c
eiblisz <eiblisz@gmail.com>
2026-09-17 11:53:05 +0200

    Install product price authority patch

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,14 @@ from . import evidence_verifier as _evidence_verifier
 from .answer_verifier_patch import (
     install_answer_price_equivalence_patch as _install_answer_price_equivalence_patch,
 )
+from .price_evidence_patch import (
+    install_price_evidence_patch as _install_price_evidence_patch,
+)
 
+_install_price_evidence_patch(_evidence_verifier)
 _install_answer_price_equivalence_patch(_evidence_verifier)
 
+del _install_price_evidence_patch
 del _install_answer_price_equivalence_patch
 del _evidence_verifier
 
